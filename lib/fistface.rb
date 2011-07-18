d1a01750cb7b19b500bd646398e00c14afe83a06
Gareth Rees <reesG12@cardiff.ac.uk>
2011-07-18 14:55:02 +0100

    Use modular extension style

--- lib/fistface.rb
+++ lib/fistface.rb
@@ -1,30 +1,38 @@
 require 'sinatra/base'
-require 'open-uri'
 
-class FistFace < Sinatra::Base
-  set :app_file, __FILE__
+module Sinatra
+  module FistFace
+    require 'open-uri'
 
-  before do
-    one_year_in_seconds = 31536000
+    def self.registered(app)
+      app.before do
+        puts "// FistFace Before Filter"
+        one_year_in_seconds = 31536000
 
-    headers 'Cache-Control'               => "public, max-age=#{one_year_in_seconds}",
-            'Expires'                     => (Time.now + one_year_in_seconds).httpdate,
-            'Access-Control-Allow-Origin' => '*'
+        headers 'Cache-Control'               => "public, max-age=#{one_year_in_seconds}",
+                'Expires'                     => (Time.now + one_year_in_seconds).httpdate,
+                'Access-Control-Allow-Origin' => '*'
+      end
+
+      app.get '/:font_face.css' do
+        puts "// FistFace Get Stylesheet"
+        headers['Content-Type'] = 'text/css'
+        open("#{ENV['S3_URL']}/#{params[:font_face]}.css").read
+      end
+
+      app.get '/:directory/:font_face' do
+        puts "// FistFace Get Font File"
+        headers['Content-Type'] = case params[:font_face]
+                                    when /\.ttf$/  then 'font/truetype'
+                                    when /\.otf$/  then 'font/opentype'
+                                    when /\.woff$/ then 'font/woff'
+                                    when /\.eot$/  then 'application/vnd.ms-fontobject'
+                                    when /\.svg$/  then 'image/svg+xml'
+                                  end
+        open("#{ENV['S3_URL']}/#{params[:directory]}/#{params[:font_face]}").read
+      end
+
+    end
   end
-
-  get '/:font_face.css' do
-    headers['Content-Type'] = 'text/css'
-    open("#{ENV['S3_URL']}/#{params[:font_face]}.css").read
-  end
-
-  get '/:directory/:font_face' do
-    headers['Content-Type'] = case params[:font_face]
-                                when /\.ttf$/  then 'font/truetype'
-                                when /\.otf$/  then 'font/opentype'
-                                when /\.woff$/ then 'font/woff'
-                                when /\.eot$/  then 'application/vnd.ms-fontobject'
-                                when /\.svg$/  then 'image/svg+xml'
-                              end
-    open("#{ENV['S3_URL']}/#{params[:directory]}/#{params[:font_face]}").read
-  end
+  register FistFace
 end
